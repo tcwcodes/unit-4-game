f3885502e64f0be63d2387771c2fc458a73048a3
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2018-04-24 18:24:07 -0500

    charactersmove

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,13 +38,59 @@ var yoshi = {
 
 var characters = [mario, luigi, peach, yoshi];
 
-$("#start-button").on("click", function(){
+$("#start-button").one("click", function(){
+
+    // Create a new div called charAll and add class "charAllClass"
+    var charAll = $("<div>");
+    charAll.addClass("charAllClass");
+    var chooseCharacters = $("<h2>" + "Choose your character" + "</h2>");
+    $("#charDiv").append(chooseCharacters);
+ 
     for (var i = 0; i < characters.length; i++) {
+
+    // Inside the charAll div, create a new div for each character, add class "ppls" to all,
+    // add unique name class, attributes and image to each
     var character = $("<div>");
     character.addClass("ppls");
+    character.addClass(characters[i].name);
+    character.attr("name", characters[i].name);
+    character.attr("attackPower", characters[i].attackPower);
+    character.attr("counterAttackPower", characters[i].counterAttackPower);
     character.append('<img src=' + characters[i].image + '>');
+
+    // Create a new div for each character's name inside of its character's div
+    var charName = $("<div>");
+    charName.addClass("name");
+    charName.text(characters[i].display);
+    character.append(charName);
+
+    // Create a new div for each character's points inside of its character's div
+    var charPoints = $("<div>");
+    charPoints.addClass("healthPoints");
+    charPoints.text(characters[i].healthPoints);
+    character.append(charPoints);
+    
+    // Append character divs to charallClass div
+    $(".charAllClass").append(character);
+
+    // Append character divs to charDiv in html
     $("#charDiv").append(character);
 };
+
+$("body").one("click", ".ppls", function(){
+    var enemyCharacters = $("<h2>" + "Your opponents" + "</h2>");
+    $("#enemyCharDiv").append(enemyCharacters);
+    $("#enemyCharDiv").append($(".ppls"));
+
+    var chosenCharacter = $("<h2>" + "Your character" + "</h2>");
+    $("#charPickedDiv").append(chosenCharacter);
+    $("#charPickedDiv").append(this);
+
+    $("#charDiv").empty();
+})
+
+
+
 
 });
 
